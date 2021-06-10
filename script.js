b165f6665a102b9520bae9ee19c31aa8a8e4f190
paanch-5 <85682335+paanch-5@users.noreply.github.com>
2021-06-11 01:50:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 function compute()
 {
-    var p = document.getElementById("principal").value;
-    var r = document.getElementById("rate").value;
-    var t = document.getElementById("years").value;
-    var si=(p*t*r)/100;
+     p = document.getElementById("principal").value;
+     r = document.getElementById("rate").value;
+     t = document.getElementById("years").value;
+     si=(p*t*r)/100;
+     result = document.getElementById("result");
     const d = new Date();
     var n = d.getFullYear();
     var y=t+n;
-    document.getElementById ('num').innerHTML ="if you deposit "+p;
-    document.getElementById ('num1').innerHTML ="at an intrest rate of "+r;
-    return y ;
+    
+                result.innerHTML ="if you deposit "+p+"<br/>"+"at an intrest rate of "+r+<br/>+"you will receive an amount of "+si+"<br/>""in the year "+y;
+       
+    
 }
 
-    document.getElementById("demo").innerHTML= "if you deposit "+p+"<br/>"+"at an intrest rate of "+r+<br/>+"you will receive an amount of "+si+"<br/>""in the year "+y;
-      
+    
