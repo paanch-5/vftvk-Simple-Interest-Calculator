c65ff3cffaabe6ee1e020231af3df9d5be22ee6b
paanch-5 <85682335+paanch-5@users.noreply.github.com>
2021-06-11 02:10:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 function compute()
 {
-     p = document.getElementById("principal").value;
-     r = document.getElementById("rate").value;
-     t = document.getElementById("years").value;
-     si=(p*t*r)/100;
-     result = document.getElementById("result");
-    const d = new Date();
-    var n = d.getFullYear();
-    var y=t+n;
-    
-                result.innerHTML ="if you deposit "+p+"<br/>"+"at an intrest rate of "+r+<br/>+"you will receive an amount of "+si+"<br/>""in the year "+y;
-       
+    var principal = document.getElementById("principal").value;
+     var rate = document.getElementById("rate").value;
+       var years = document.getElementById("years").value;
+      var interest = principal * years * rate /100;
+         var year = new Date().getFullYear()+parseInt(years);
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+} 
+     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+     
     
 }
 
