5de5d98996e5fd03723be4ac991e77f6b7969b1c
paanch-5 <85682335+paanch-5@users.noreply.github.com>
2021-06-11 01:20:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function compute()
     const d = new Date();
     var n = d.getFullYear();
     var y=t+n;
-    document.getElementById("demo").innerHTML= "if you deposit "+p+"<br/>"+"at an intrest rate of "+r+<br/>+"you will receive an amount of "+si+"<br/>""in the year "+y;
-
-    
+    return y ;
 }
-        
+
+    document.getElementById("demo").innerHTML= "if you deposit "+p+"<br/>"+"at an intrest rate of "+r+<br/>+"you will receive an amount of "+si+"<br/>""in the year "+y;
+      
